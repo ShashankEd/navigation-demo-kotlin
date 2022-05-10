6bc57f11537721c36adb32b678eca84649807e24
Shashank Gupta <shashank.gupta006@gmail.com>
2022-05-11 01:24:00 +0530

    Added third fragment, and it has action to navigated back to first fragment

--- app/src/main/java/com/example/simplenavigationappdemo/ThirdFragment.kt
+++ app/src/main/java/com/example/simplenavigationappdemo/ThirdFragment.kt
@@ -7,9 +7,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.navArgs
-import com.example.simplenavigationappdemo.ui.main.MainFragmentDirections
 import kotlinx.android.synthetic.main.fragment_second.*
-import kotlinx.android.synthetic.main.main_fragment.*
+import kotlinx.android.synthetic.main.fragment_third.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,15 +17,15 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [SecondFragment.newInstance] factory method to
+ * Use the [ThirdFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class SecondFragment : Fragment() {
+class ThirdFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
     //to get the arguments from first fragment we'll have to use SecondFragmentArgs
-    private val args: SecondFragmentArgs by navArgs<SecondFragmentArgs>()
+    private val args: ThirdFragmentArgs by navArgs<ThirdFragmentArgs>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,11 +37,11 @@ class SecondFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        //consume the args like below
-        textView.text = args.username
 
-        buttonSecond.setOnClickListener {
-            Navigation.findNavController(it).navigate(SecondFragmentDirections.secondToThird(useraage = ageText.text.toString()))
+        ageFillText.text = args!!.useraage
+
+        gotToFirstBtn.setOnClickListener {
+            Navigation.findNavController(it).navigate(ThirdFragmentDirections.thirdToMain())
         }
     }
 
@@ -51,7 +50,7 @@ class SecondFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false)
+        return inflater.inflate(R.layout.fragment_third, container, false)
     }
 
     companion object {
@@ -61,17 +60,16 @@ class SecondFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment SecondFragment.
+         * @return A new instance of fragment ThirdFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            SecondFragment().apply {
+            ThirdFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
                 }
             }
     }
-
 }
